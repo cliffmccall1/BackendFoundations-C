2efae3ddb624072947ce444dd1683f8ca80aceff
Cliff McCall <cliff.mccall1@gmail.com>
2018-11-30 17:07:59 -0600

    initial commit

---
+++ CSharp/PartyRSVP/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyRSVP.Models;
+
+namespace PartyRSVP.Conrollers {
+    public class HomeController : Controller {
+        public ViewResult Index () {
+            return View ("Rsvp");
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm (GuestResponse guestResponse) {
+            // Todo: Store guest response, covered in later lessons
+            return View ("Thanks", guestResponse);
+        }
+    }
+}
